c6648dbeca118e951659111ace2613a9e121479e
Ricardo Marschall <ricardo.marschall@rixtrem.com>
2015-05-13 22:37:26 +0000

    initial commit uploadtest

---
+++ upload.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+//Path to autoload.php from current location
+require_once './vendor/autoload.php';
+
+$config = new \Flow\Config();
+$config->setTempDir('./chunks_temp_folder');
+$request = new \Flow\Request();
+if (\Flow\Basic::save('./' . $request->getIdentifier(), $config, $request)) {
+  // file saved successfully and can be accessed at './final_file_destination'
+} else {
+  // This is not a final chunk or request is invalid, continue to upload.
+}
